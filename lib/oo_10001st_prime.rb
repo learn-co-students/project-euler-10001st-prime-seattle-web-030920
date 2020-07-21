12eff324d36b02b25e9ef668418deca3dc70f569
SaraKhandaker <kttsara@gmail.com>
2020-07-21 16:51:23 -0700

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class Prime
+    def initialize(number)
+        @number=number
+    end
+
+    def number
+        array=[2]
+        i=3
+        while array.length < @number
+            if isPrime(i)
+                array.push(i) 
+            end
+            i+=1
+        end
+      array.pop()
+    end
+    
+    def isPrime (n)
+        (2..Math.sqrt(n)).none? {|f| n % f == 0}
+    end
+end
